5b7f51ca9f0ddce05406e5bab294a58f266cda0a
Alex <alexrgura@gmail.com>
2019-01-22 15:54:12 -0600

    dealing with pg error

---
+++ models.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+
+const plantrDB = new Sequelize('postgres://localhost:5432/plantr')
+
+const Gardener = plantrDB.define('gardener', {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+})
+
+const Plot = plantrDB.define('plot', {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+})
+
+const Vegetable = plantrDB.define('vegetable', {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
+})
+
+
+
+Plot.belongsTo(Gardener);
+Gardener.hasOne(Plot);
+
+Vegetable.belongsToMany(Plot, {through: vegetable_plot})
+Plot.belongsToMany(Vegetable, {through: vegetable_plot})
+Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'})
+
+module.exports = plantrDB
--- seed.js
+++ seed.js
@@ -1,1 +1,11 @@
-dsdasddsfsd
+const db = require('./models');
+
+db.sync({ force: true })
+  .then(function() {
+    console.log('success');
+    db.close();
+  })
+  .catch(function() {
+    console.log('failure')
+    db.close();
+  });
